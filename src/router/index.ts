d1e3d79d15e6348c149ed420603d552b44dd5346
zhangbaozheng <488937163@qq.com>
2020-10-20 08:16:27 +0800

    'day1'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,51 @@
-import Home from '@/views/Home'
-import Login from '@/views/Login'
-
+/*
+ * @Author: your name
+ * @Date: 2020-10-19 18:59:55
+ * @LastEditTime: 2020-10-19 19:59:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \exam-L\src\router\index.ts
+ */
+import Home from "@/views/Home";
+import Login from "@/views/Login";
+import UserContol from '../views/userContol'
+import UserAdd from "../views/userControl/userAdd"
+import UserShow from "../views/userControl/userShow"
 const routes = [
-    {
-        path: '/',
-        redirect: '/index',
-        name: '/'
-    },
-    {
-        path: '/index',
-        name: 'index',
-        component: Home,
+  {
+    path: "/",
+    redirect: "/index",
+    name: "/",
+  },
+  {
+    path: "/index",
+    name: "index",
+    component: Home,
+    children: [
+      {
+        path:'/index/usercontol',
+        component:UserContol,
+        name: "用户管理",
         children: [
-            {
-                name: '123'
-            }
-        ]
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login
-    }
-]
+          {
+            path:'/index/usercontol/useradd',
+            name: "添加用户",
+            component:UserAdd
+          },
+          {
+            path:'/index/usercontol/usershow',
+            name: "用户展示",
+            component:UserShow
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+];
 
 export default routes;
-
